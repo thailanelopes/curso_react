3e247afbb21906f89cf3336da1ef887394b559d7
Thailane Dutra <thailane.dutra@ibm.com>
2023-03-29 21:59:39 -0300

    criando componentes

---
+++ curso-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Comentario from './components/Comentario';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gatariaaa miau miau caralho</h1>
+      <Comentario/>
+      <Comentario/>
+    </div>
+  );
+}
+
+export default App;
---
+++ curso-react/src/components/Comentario.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './Comentario.css'
+//jsx
+const Comentario = () => (
+    <div className="Comentario">
+     <div>Componente</div>
+     </div>
+);
+
+export default Comentario;
