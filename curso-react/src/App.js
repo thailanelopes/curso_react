347c04a8c7262d1186851181a01f7b4d2f690ba1
thailane.dutra <thailane111122@gmail.com>
2023-03-31 12:09:25 -0300

    estado da aplicacao

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Comentario from './components/Comentario';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Gatariaaa miau miau caralho</h1>
-      <Comentario nome="Joao" email="joao@gmail.com" data={new Date(2023, 3, 29)}>
-        Eu sou um estudante da gataria.
-        </Comentario>
-      <Comentario nome="Maria" email="maria@gmail.com" data={new Date(2023, 3, 29)}>
-      gataria is my religion
-        </Comentario>
-    </div>
-  );
+  state = {
+    coments: [
+      {
+        nome: 'Joao',
+        email: 'joao@gmail.com',
+        data: new Date(2023, 3, 29),
+        mensagem: 'Eu sou um estudante da gataria.'
+      },
+
+      {
+        nome: 'Maria',
+        email: 'maria@gmail.com',
+        data: new Date(2023, 3, 29),
+        mensagem: 'Eu sou um estudante da gataria.'
+      }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Gatariaaa miau miau caralho</h1>
+
+        {this.state.coments.map((comentario, indice) => (
+      <Comentario
+        key= {indice}
+        nome= {comentario.nome}
+        email= {comentario.email} 
+        data= {comentario.data}>
+       {comentario.mensagem}
+      </Comentario>
+        ))}
+      </div>
+    ); 
+  }
 }
 
 export default App;
--- curso-react/src/components/Comentario.js
+++ curso-react/src/components/Comentario.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './Comentario.css';
 //jsx
-const Comentario = props => (
-    <div className="Comentario">
+const Comentario = props => {
+
+    return <div className="Comentario">
      <h2>{props.nome}</h2>
      <p>{props.email}</p>
      <p>{props.children}</p>
      <p>{props.data.toString()}</p>
      </div>
-);
+};
 
 export default Comentario;
